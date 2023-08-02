221ec524a33e2e7d9dd3dea100c9945963eb3850
∞ infi <106063205+InfiniteGwei@users.noreply.github.com>
2023-08-02 13:52:47 +0530

    Update guildPreferences.js

--- src/models/GuildPreferences.js
+++ src/models/GuildPreferences.js
@@ -1,14 +1,30 @@
-const {Schema, model} = require('mongoose');
+const { Schema, model } = require('mongoose');
 
-const GuildPreferencesSchema = new Schema({
-  guildId: { type: String, required: true, unique: true },
-  baseXp: { type: Number, required: false },
-  levels: { type: Number, required: false },
-  levelingSchema: [{
-    level: Number,
-    minXP: Number,
-    roleId: String,
-  }],
+const serverSettingsSchema = new Schema({
+  guildId: {
+    type: String,
+    required: true,
+  },
+  xpPerReaction: {
+    type: Number,
+    default: 10,
+  },
+  reactionEmojis: {
+    type: [String],
+    default: [],
+  },
+  rolesCanGiveXP: {
+    type: [String],
+    default: [],
+  },
+  levelingSchema: {
+    type: [{ level: Number, xpThreshold: Number, role: { type: String, default: null } }],
+    default: [],
+  },
+  adminPassword: {
+    type: String,
+    required: true,
+  },
 });
 
-module.exports = model('GuildPreferences', GuildPreferencesSchema);
+module.exports = model('ServerSettings', serverSettingsSchema);
